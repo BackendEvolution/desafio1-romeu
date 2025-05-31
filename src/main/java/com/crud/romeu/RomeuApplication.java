910dd654c26613b02bd7f8aa02233d0d4f5106df
romeu cajamba <romeucajamba@gmail.com>
2025-05-31 13:45:41 +0100

    desafio inicializado e preparado

---
+++ src/main/java/com/crud/romeu/RomeuApplication.java
@@ -0,0 +1,13 @@
+package com.crud.romeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomeuApplication.class, args);
+	}
+
+}
