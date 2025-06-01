a73c9770ecfa481703bb5ffc7199a73e6ba8c399
romeu cajamba <romeucajamba@gmail.com>
2025-06-01 03:05:36 +0100

    fix:correcção de erro de importação e anotação

--- src/main/java/com/crud/romeu/domain/User.java
+++ src/main/java/com/crud/romeu/domain/User.java
@@ -6,10 +6,13 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import jakarta.persistence.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
